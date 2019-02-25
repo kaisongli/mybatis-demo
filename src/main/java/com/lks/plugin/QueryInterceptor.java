20de665cfb93bbbf9d07589325b60d2a1a2c5ac1
黎开松 <likaisong@meituan.com>
2019-02-26 00:40:49 +0800

    实现自定义拦截器和优化TestMain

---
+++ src/main/java/com/lks/plugin/QueryInterceptor.java
@@ -0,0 +1,56 @@
+package com.lks.plugin;
+
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+
+import java.util.Properties;
+
+/**
+ * Created by likaisong on 2019/2/25.
+ */
+@Intercepts(
+        @Signature(method = "query" ,
+        type = Executor.class,
+                args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class})
+)
+public class QueryInterceptor implements Interceptor {
+    /**
+     * 拦截时执行的操作
+     *
+     * @param invocation { 代理对象，被监控方法对象，当前被监控方法运行时需要的实参 }
+     * @return
+     * @throws Throwable
+     */
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        System.out.println("被拦截方法执行前的操作。。。");
+        Object proceed = invocation.proceed();
+        System.out.println("被拦截方法执行后的操作。。。");
+        return proceed;
+    }
+
+    /**
+     * 拦截器用于封装目标对象的
+     * 通过该方法我们可以返回目标对象本身，也可以返回一个它的代理
+     *
+     * @param o
+     * @return
+     */
+    @Override
+    public Object plugin(Object o) {
+        return Plugin.wrap(o, this);
+    }
+
+    /**
+     * 用于在 Mybatis 配置文件中指定一些属性的。
+     *
+     * @param properties
+     */
+    @Override
+    public void setProperties(Properties properties) {
+
+    }
+}
--- src/test/java/com/lks/test/TestMain.java
+++ src/test/java/com/lks/test/TestMain.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -16,9 +17,10 @@ import java.util.Date;
  * Created by likaisong on 2019/2/24.
  */
 public class TestMain {
+    private static SqlSession session;
 
-    private static SqlSession getSqlSession(String resource){
-        if (resource == null || "".equals(resource)){
+    private static SqlSession getSqlSession(String resource) {
+        if (resource == null || "".equals(resource)) {
             return null;
         }
         InputStream inputStream = null;
@@ -28,188 +30,181 @@ public class TestMain {
             e.printStackTrace();
         }
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        SqlSession session = sqlSessionFactory.openSession();
+        session = sqlSessionFactory.openSession();
         return session;
     }
+
     @Test
-    public static void testSelectByXml(){
+    public static void testSelectByXml() {
         String resource = "mybatis-config.xml";
-        SqlSession session = getSqlSession(resource);
-        try {
-            String statement = "userMapper.getUser";
-            User user = session.selectOne(statement, 2);
-            System.out.println(user);
-        } finally {
-            session.close();
-        }
+        session = getSqlSession(resource);
+        String statement = "userMapper.getUser";
+        User user = session.selectOne(statement, 2);
+        System.out.println(user);
     }
 
     @Test
-    public static void testInsertByXml(){
+    public static void testInsertByXml() {
         String resource = "mybatis-config.xml";
-        SqlSession session = getSqlSession(resource);
-        try {
-            String statement = "userMapper.insertUser";
-            User user = new User();
-            user.setName("阿飞");
-            user.setAge(20);
-            user.setCounty("中国");
-            session.insert(statement, user);
-            session.commit();
-            System.out.println("插入数据后返回的自增ID：" + user.getId());
-        } finally {
-            session.close();
-        }
+        session = getSqlSession(resource);
+
+        String statement = "userMapper.insertUser";
+        User user = new User();
+        user.setName("阿飞");
+        user.setAge(20);
+        user.setCounty("中国");
+        session.insert(statement, user);
+        session.commit();
+        System.out.println("插入数据后返回的自增ID：" + user.getId());
+
     }
 
     @Test
-    public static void testUpdateByXml(){
+    public static void testUpdateByXml() {
         String resource = "mybatis-config.xml";
-        SqlSession session = getSqlSession(resource);
-        try {
-            String statement = "userMapper.updateUser";
-            User user = new User();
-            user.setId(1);
-            user.setName("小李飞刀");
-            session.update(statement, user);
-            session.commit();
-        } finally {
-            session.close();
-        }
+        session = getSqlSession(resource);
+
+        String statement = "userMapper.updateUser";
+        User user = new User();
+        user.setId(1);
+        user.setName("小李飞刀");
+        session.update(statement, user);
+        session.commit();
+
     }
 
     @Test
-    public static void testDeleteByXml(){
+    public static void testDeleteByXml() {
         String resource = "mybatis-config.xml";
-        SqlSession session = getSqlSession(resource);
-        try {
-            //先增加一条数据
-            String insertStatement = "userMapper.insertUser";
-            User user = new User();
-            user.setName("阿飞");
-            user.setAge(20);
-            user.setCounty("中国");
-            session.insert(insertStatement, user);
-            System.out.println("插入数据后返回的自增ID：" + user.getId());
-            //再删掉刚刚加的数据
-            String deleteStatement = "userMapper.deleteUser";
-            session.delete(deleteStatement, user.getId());
-            session.commit();
-        } finally {
-            session.close();
-        }
+        session = getSqlSession(resource);
+
+        //先增加一条数据
+        String insertStatement = "userMapper.insertUser";
+        User user = new User();
+        user.setName("阿飞");
+        user.setAge(20);
+        user.setCounty("中国");
+        session.insert(insertStatement, user);
+        System.out.println("插入数据后返回的自增ID：" + user.getId());
+        //再删掉刚刚加的数据
+        String deleteStatement = "userMapper.deleteUser";
+        session.delete(deleteStatement, user.getId());
+        session.commit();
+
     }
 
 
     @Test
-    public static void testSelectByAnnotation(){
+    public static void testSelectByAnnotation() {
         String resource = "mybatis-annotation-config.xml";
-        SqlSession session = getSqlSession(resource);
-        try {
-            UserMapper mapper = session.getMapper(UserMapper.class);
-            User user = mapper.getUser(1);
-            System.out.println(user);
-        } finally {
-            session.close();
-        }
+        session = getSqlSession(resource);
+
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        User user = mapper.getUser(1);
+        System.out.println(user);
+
     }
 
     @Test
-    public static void testUpdateByAnnotation(){
+    public static void testUpdateByAnnotation() {
         String resource = "mybatis-annotation-config.xml";
-        SqlSession session = getSqlSession(resource);
-        try {
-            UserMapper mapper = session.getMapper(UserMapper.class);
-            User user = new User();
-            user.setId(2);
-            user.setName("钢铁侠");
-            mapper.updateUser(user);
-            session.commit();
-        } finally {
-            session.close();
-        }
+        session = getSqlSession(resource);
+
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        User user = new User();
+        user.setId(2);
+        user.setName("钢铁侠");
+        mapper.updateUser(user);
+        session.commit();
+
     }
 
     @Test
-    public static void testInsertByAnnotation(){
+    public static void testInsertByAnnotation() {
         String resource = "mybatis-annotation-config.xml";
-        SqlSession session = getSqlSession(resource);
-        try {
-            UserMapper mapper = session.getMapper(UserMapper.class);
-            User user = new User();
-            user.setName("阿飞");
-            user.setAge(20);
-            user.setCounty("中国");
-            mapper.insertUser(user);
-            session.commit();
-            System.out.println("插入数据后返回的自增ID：" + user.getId());
-        } finally {
-            session.close();
-        }
+        session = getSqlSession(resource);
+
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        User user = new User();
+        user.setName("阿飞");
+        user.setAge(20);
+        user.setCounty("中国");
+        mapper.insertUser(user);
+        session.commit();
+        System.out.println("插入数据后返回的自增ID：" + user.getId());
+
     }
 
     @Test
-    public static void testDeleteByAnnotation(){
+    public static void testDeleteByAnnotation() {
         String resource = "mybatis-annotation-config.xml";
-        SqlSession session = getSqlSession(resource);
-        try {
-            //先添加一条数据
-            UserMapper mapper = session.getMapper(UserMapper.class);
-            User user = new User();
-            user.setName("阿飞");
-            user.setAge(20);
-            user.setCounty("中国");
-            mapper.insertUser(user);
-            System.out.println("插入数据后返回的自增ID：" + user.getId());
-            //再删除刚加的数据
-            mapper.deleteUser(user.getId());
-            session.commit();
-        } finally {
-            session.close();
-        }
+        session = getSqlSession(resource);
+
+        //先添加一条数据
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        User user = new User();
+        user.setName("阿飞");
+        user.setAge(20);
+        user.setCounty("中国");
+        mapper.insertUser(user);
+        System.out.println("插入数据后返回的自增ID：" + user.getId());
+        //再删除刚加的数据
+        mapper.deleteUser(user.getId());
+        session.commit();
+
     }
 
     @Test
-    public static void testDateToString(){
+    public static void testDateToString() {
         String resource = "mybatis-config.xml";
-        SqlSession session = getSqlSession(resource);
-        try {
-            String statement = "userMapper.dateToString";
-            User user = new User();
-            user.setName("阿飞");
-            user.setAge(20);
-            user.setCounty("中国");
-            user.setDate(new Date());
-            session.insert(statement, user);
-            session.commit();
-            System.out.println("插入数据后返回的自增ID：" + user.getId());
-        } finally {
-            session.close();
-        }
+        session = getSqlSession(resource);
+
+        String statement = "userMapper.dateToString";
+        User user = new User();
+        user.setName("阿飞");
+        user.setAge(20);
+        user.setCounty("中国");
+        user.setDate(new Date());
+        session.insert(statement, user);
+        session.commit();
+        System.out.println("插入数据后返回的自增ID：" + user.getId());
+
     }
 
     @Test
-    public static void testStringToDate(){
+    public static void testStringToDate() {
         String resource = "mybatis-config.xml";
-        SqlSession session = getSqlSession(resource);
-        try {
-            //先插入一条数据
-            String insertStatement = "userMapper.dateToString";
-            User user = new User();
-            user.setName("阿飞");
-            user.setAge(20);
-            user.setCounty("中国");
-            user.setDate(new Date());
-            session.insert(insertStatement, user);
-            session.commit();
-            System.out.println("插入数据后返回的自增ID：" + user.getId());
-            //查询刚插入的数据
-            String selectStatement = "userMapper.stringToDate";
-            User resUser = session.selectOne(selectStatement, user.getId());
-            System.out.println(resUser);
-        } finally {
-            session.close();
-        }
+        session = getSqlSession(resource);
+
+        //先插入一条数据
+        String insertStatement = "userMapper.dateToString";
+        User user = new User();
+        user.setName("阿飞");
+        user.setAge(20);
+        user.setCounty("中国");
+        user.setDate(new Date());
+        session.insert(insertStatement, user);
+        session.commit();
+        System.out.println("插入数据后返回的自增ID：" + user.getId());
+        //查询刚插入的数据
+        String selectStatement = "userMapper.stringToDate";
+        User resUser = session.selectOne(selectStatement, user.getId());
+        System.out.println(resUser);
+
+    }
+
+    @Test
+    public static void testQueryIntercepor() {
+        String resource = "mybatis-config.xml";
+        session = getSqlSession(resource);
+        String statement = "userMapper.queryInterceptor";
+        User user = session.selectOne(statement, 1);
+        System.out.println(user);
+    }
+
+    @AfterTest
+    public static void closeSession() {
+        session.close();
     }
 
 }
